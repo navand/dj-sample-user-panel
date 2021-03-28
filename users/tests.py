d8684bb90080b2f6084a9f492b21c8785090666a
Navand Namiranian <navandn@gmail.com>
2021-03-28 11:47:31 +0430

    Create tests for users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth import (
+    BACKEND_SESSION_KEY, SESSION_KEY, _clean_credentials, authenticate,
+    get_user, signals,
+)
 
-# Create your tests here.
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='client', password='admin')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username='client', password='admin')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='wrong', password='admin')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_password(self):
+        user = authenticate(username='client', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
